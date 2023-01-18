e84a8a5123d5bd5945f7087049e15767308e776d
Lucas Nørgård <lucasnrgaard@gmail.com>
2023-01-18 22:36:19 +0100

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,81 @@
+import { statSync } from "node:fs";
+import { stat } from "node:fs/promises";
+import { parse, resolve } from "node:path";
+
+export interface Options {
+  /**
+   * The directory to start searching from.
+   * @default process.cwd()
+   */
+  cwd?: string;
+
+  /**
+   * The path to the directory to stop searching at.
+   * @default path.parse(cwd).root
+   */
+  stop?: string;
+
+  /**
+   * Test the file before returning it.
+   */
+  test?: (file: string) => boolean | Promise<boolean>;
+}
+
+export async function find(
+  name: string | Array<string>,
+  options?: Options
+): Promise<string | null> {
+  let dir = resolve(options?.cwd || process.cwd());
+
+  const root = options?.stop || parse(dir).root;
+  const fileNames = Array.isArray(name) ? name : [name];
+
+  while (dir !== root) {
+    for (const name of fileNames) {
+      const file = resolve(dir, name);
+      try {
+        await stat(file);
+        if (!options?.test || (await options.test(file))) return file;
+      } catch (e) {
+        if (e.code !== "ENOENT") {
+          throw e;
+        }
+      }
+    }
+    dir = parse(dir).dir;
+  }
+  return null;
+}
+
+export function findSync(
+  name: string | Array<string>,
+  options?: Options
+): string | null {
+  let dir = resolve(options?.cwd || process.cwd());
+  const root = options?.stop || parse(dir).root;
+  const fileNames = Array.isArray(name) ? name : [name];
+
+  while (dir !== root) {
+    for (const name of fileNames) {
+      const file = resolve(dir, name);
+      try {
+        statSync(file);
+        const test = options?.test || (() => true);
+        if (
+          Object.getPrototypeOf(test).constructor.name ===
+          "AsyncFunction"
+        ) {
+          throw new TypeError("You are using a async test in sync mode.");
+        }
+
+        if (!test || test(file)) return file;
+      } catch (e) {
+        if (e.code !== "ENOENT") {
+          throw e;
+        }
+      }
+    }
+    dir = parse(dir).dir;
+  }
+  return null;
+}
---
+++ tests/index.test.ts
@@ -0,0 +1,98 @@
+import { readFileSync } from "node:fs";
+import { readFile } from "node:fs/promises";
+import { basename, join } from "node:path";
+
+import { describe, expect, test } from "vitest";
+
+import { find, findSync } from "../src";
+
+describe("find", () => {
+  test("find minions", async () => {
+    const result = await find(["minions.jpg", "package.json"], {
+      cwd: join(__dirname, "fixture", "a", "b")
+    });
+
+    expect(result).not.toBe(join(__dirname, "fixture", "package.json"));
+    expect(result).toBe(join(__dirname, "fixture", "minions.jpg"));
+  });
+
+  test("find package.json", async () => {
+    const result = await find("package.json", {
+      cwd: join(__dirname, "fixture", "a", "b")
+    });
+
+    expect(result).toBe(join(__dirname, "fixture", "package.json"));
+    expect(result).not.toBe(join(__dirname, "fixture", "minions.jpg"));
+  });
+
+  test("find package.json with key", async () => {
+    const result = await find("package.json", {
+      cwd: join(__dirname, "fixture", "a", "b"),
+      async test(file) {
+        const base = basename(file);
+        if (base === "package.json") {
+          const content = JSON.parse(await readFile(file, "utf-8"));
+          return content.version;
+        }
+        return false;
+      }
+    });
+
+    expect(result).toBe(join(__dirname, "fixture", "package.json"));
+  });
+});
+
+describe("findSync", () => {
+  test("find minions", () => {
+    const result = findSync(["minions.jpg", "package.json"], {
+      cwd: join(__dirname, "fixture", "a", "b")
+    });
+
+    expect(result).not.toBe(join(__dirname, "fixture", "package.json"));
+    expect(result).toBe(join(__dirname, "fixture", "minions.jpg"));
+  });
+
+  test("find package.json", () => {
+    const result = findSync("package.json", {
+      cwd: join(__dirname, "fixture", "a", "b")
+    });
+
+    expect(result).toBe(join(__dirname, "fixture", "package.json"));
+    expect(result).not.toBe(join(__dirname, "fixture", "minions.jpg"));
+  });
+
+  test("throw error when using async in test inside sync", () => {
+    const result = () =>
+      findSync("package.json", {
+        cwd: join(__dirname, "fixture", "a", "b"),
+        async test(file) {
+          const base = basename(file);
+          if (base === "package.json") {
+            const content = JSON.parse(await readFile(file, "utf-8"));
+            return content.version;
+          }
+          return false;
+        }
+      });
+
+    expect(() => result()).toThrowError(
+      /^You are using a async test in sync mode.$/
+    );
+  });
+
+  test("find package.json with key", () => {
+    const result = findSync("package.json", {
+      cwd: join(__dirname, "fixture", "a", "b"),
+      test(file) {
+        const base = basename(file);
+        if (base === "package.json") {
+          const content = JSON.parse(readFileSync(file, "utf-8"));
+          return content.version;
+        }
+        return false;
+      }
+    });
+
+    expect(result).toBe(join(__dirname, "fixture", "package.json"));
+  });
+});
